9a2a0f6b5ad18c1b5f9569ae5f42e6c36ea3f26f
Tseing <im.yczeng@foxmail.com>
2020-06-18 18:46:09 +0800

    :zap: finish exp_8

--- Exp8.py
+++ Exp8.py
@@ -4,22 +4,24 @@ import matplotlib.pyplot as plt
 from matplotlib import rcParams
 
 c = np.array([5, 3.33, 2.5, 2, 1.43])
-#r = np.array([1.4687, 1.2533, 1.2163, 1.1593, 1.1135])
-#sp = np.array([0.4687, 0.2533, 0.2163, 0.1593, 0.1135])
-
 t0 = 89.14
 t = np.array([149.50, 126.30, 115.38, 109.96, 103.66])
 r = t/t0
 sp = r - 1
-print(sp)
 
 rfit = np.poly1d(np.polyfit(c, np.log(r)/c, 1))
-#spfit = np.poly1d(np.polyfit(c, sigma, 1))
-line_x=[0,6]
+spfit = np.poly1d(np.polyfit(c, sp/c, 1))
+print("rfit = ", rfit)
+print("spfit = ", spfit)
+line_x=[0,6.5]
 
 plt.xlim(0, 6.5)
+plt.xlabel(r'$c\mathrm{/kg\cdot m^{-3}}$', fontsize=14)
+plt.ylabel(r'$\frac{\eta_r}{c}\ \ $or$\ \ \ln{\frac{\eta_{sp}}{c}}$', fontsize=14)
 plt.scatter(c, np.log(r)/c)
-#plt.scatter(c, np.log(sp)/c)
-plt.plot(line_x, rfit(line_x), c='r')
+plt.scatter(c, sp/c)
+plt.plot(line_x, rfit(line_x), label=r"$\frac{\eta_r}{c}=-0.0004739c+0.1057$")
+plt.plot(line_x, spfit(line_x), label=r"$\ln{\frac{\eta_{sp}}{c}}=0.006198c+0.1041$")
 
+plt.legend(fontsize=14)
 plt.show()
