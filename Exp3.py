1a86646218f096125063f501d4e7da300972b122
Tseing <im.yczeng@foxmail.com>
2020-06-18 10:19:11 +0800

    :hammer: refactor exp_3

--- Exp3.py
+++ Exp3.py
@@ -13,33 +13,31 @@ plt.rcParams['font.serif'] = ['Times New Roman']
 
 k_01 = 1530
 k_02 = 1587
+kinf_1 = 745
+kinf_2 = 803
 t = np.array([4, 8, 12, 16, 20, 24, 28, 32, 36, 40])
 kt_1 = np.array([1402, 1360, 1323, 1291, 1261, 1235, 1212, 1190, 1170, 1152])
 kt_2 = np.array([1542, 1452, 1381, 1324, 1277, 1238, 1204, 1176, 1151, 1129])
-t = t * 60
-k_1 = (k_01 - kt_1)/t
-k_2 = (k_02 - kt_2)/t
+k_1 = (k_01 - kt_1)/(kt_1 - kinf_1)
+k_2 = (k_02 - kt_2)/(kt_2 - kinf_2)
 
-
-fit_1 = np.poly1d(np.polyfit(k_1[1:], kt_1[1:], 1))
-fit_2 = np.poly1d(np.polyfit(k_2[1:], kt_2[1:], 1))
+fit_1 = np.poly1d(np.polyfit(t, k_1, 1))
+fit_2 = np.poly1d(np.polyfit(t, k_2, 1))
 print('func_1=', fit_1)
 print('func_2=', fit_2)
+print(k_1)
+print(k_2)
 
-plt.xlabel(r'$\frac{\kappa_0-\kappa_{t}}{t}\mathrm{/\mu S\cdot cm^{-1}\cdot s^{-1}}$', fontsize=14)
-plt.ylabel(r'$\kappa_{t}\mathrm{/\mu S\cdot cm^{-1}}$', fontsize=14)
+line_x = [0, 45]
+plt.scatter(t, k_1, label=r'$\mathrm{T=298.15K}$')
+plt.plot(line_x, fit_1(line_x),
+         label=r'$\frac{\kappa_0-\kappa_t}{\kappa_t-\kappa_\infty}=0.02036t+0.1131$')
+plt.scatter(t, k_2, label=r'$\mathrm{T=308.15K}$')
+plt.plot(line_x, fit_2(line_x),
+         label=r'$\frac{\kappa_0-\kappa_t}{\kappa_t-\kappa_\infty}=0.03733t+0.0911$')
 
+plt.xlabel(r'$t\mathrm{/min}$', fontsize=14)
+plt.ylabel(r'$\frac{\kappa_0-\kappa_t}{\kappa_t-\kappa_\infty}$', fontsize=14)
 
-line_x1 = [0.15, 0.5]
-plt.scatter(k_1, kt_1, label='measuring data')
-plt.plot(line_x1, fit_1(line_x1), c='r', label='$y=1072x+1004$')
-print(k_1, kt_1)
-
-line_x2 = [0.175, 0.3]
-#plt.scatter(k_2, kt_2, label='measuring data')
-#plt.plot(line_x2, fit_2(line_x2), c='r', label='$y=2945x+546.8$')
-#print(k_2, kt_2)
-
-plt.legend(fontsize=14, loc='lower right')
-
+plt.legend(fontsize=13)
 plt.show()
---
+++ Exp8.py
@@ -0,0 +1,33 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import rcParams
+
+config = {
+    "font.family":'serif',
+    "mathtext.fontset":'stix',
+    "font.serif": ['SimSun'],
+    'figure.dpi':'140',
+}
+rcParams.update(config)
+plt.rcParams['font.serif'] = ['Times New Roman']
+
+c = np.array([5, 3.33, 2.5, 2, 1.43])
+#r = np.array([1.4687, 1.2533, 1.2163, 1.1593, 1.1135])
+#sp = np.array([0.4687, 0.2533, 0.2163, 0.1593, 0.1135])
+
+t0 = 89.14
+t = np.array([149.50, 126.30, 115.38, 109.96, 103.66])
+r = t/t0
+sp = r - 1
+print(sp)
+
+rfit = np.poly1d(np.polyfit(c, np.log(r)/c, 1))
+#spfit = np.poly1d(np.polyfit(c, sigma, 1))
+line_x=[0,6]
+
+plt.xlim(0, 6.5)
+plt.scatter(c, np.log(r)/c)
+#plt.scatter(c, np.log(sp)/c)
+plt.plot(line_x, rfit(line_x), c='r')
+
+plt.show()
